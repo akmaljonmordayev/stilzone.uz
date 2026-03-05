3717f835cb845ebe4fb65f14ea8ea03cee095769
jojojijo302 <MUSTANGUZB170@gmail.com>
2026-03-05 16:51:37 +0500

    button

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,19 +1,56 @@
 import React from "react";
 import "./Button.css";
 
-function Button({ btnText, bgColor, p, image, w, icon }) {
+function Button({
+  btnText,
+  bgColor = "linear-gradient(90deg,#3aa0e6,#2f86c9)",
+  textColor = "#fff",
+  w = "auto",
+  p = "14px 28px",
+  rounded = "40px",
+  icon,
+  image,
+  iconPosition = "left",
+  onClick,
+  type = "button",
+  disabled = false,
+  loading = false,
+  className = "",
+  shadow = true
+}) {
   return (
     <button
-      className="btn"
-      style={{ width: w, padding: p, backgroundColor: bgColor }}
+      type={type}
+      disabled={disabled || loading}
+      onClick={onClick}
+      className={`btn ${shadow ? "btn-shadow" : ""} ${className}`}
+      style={{
+        width: w,
+        padding: p,
+        background: bgColor,
+        color: textColor,
+        borderRadius: rounded
+      }}
     >
-      {image && <img src={image} alt="btn-image" className="btn-img" />}
+      {loading ? (
+        <span className="btn-loading">Loading...</span>
+      ) : (
+        <>
+          {icon && iconPosition === "left" && (
+            <span className="btn-icon">{icon}</span>
+          )}
+          {image && <img src={image} alt="btn" className="btn-img" />}
 
-      {btnText && <span className="btn-text">{btnText}</span>}
+          {btnText && <span className="btn-text">{btnText}</span>}
 
-      {icon && <span className="btn-icon">{icon}</span>}
+          {icon && iconPosition === "right" && (
+            <span className="btn-icon">{icon}</span>
+          )}
+        </>
+      )}
     </button>
   );
 }
+
 
 export default Button;
